8a7c6a5bda0929712304822061439f406b3ac96b
riddhisolanki001 <riddhi@sanskartechnolab.com>
2026-01-23 19:09:13 +0530

    Sales invoice validation

--- lexta/hooks.py
+++ lexta/hooks.py
@@ -137,13 +137,11 @@ app_license = "mit"
 # ---------------
 # Hook on document methods and events
 
-# doc_events = {
-# 	"*": {
-# 		"on_update": "method",
-# 		"on_cancel": "method",
-# 		"on_trash": "method"
-# 	}
-# }
+doc_events = {
+  "Sales Invoice": {
+       "validate": "lexta.api.sales_invoice.collect_all_negative_stock_errors",
+    }
+ }
 
 # Scheduled Tasks
 # ---------------
